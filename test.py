5c20d576ff7f6c8db4639b48f1f6b9ea8a56f25a
rocky <naaku78@hotmail.com>
2022-03-20 00:41:26 -0500

    Saving updates

--- test.py
+++ test.py
@@ -1,2 +1,58 @@
 
-import pandas as pd
+#import library
+from importlib import path
+import os
+import csv
+
+#joining path
+budget_data = os.path.join("../Resources/budget_data.csv")
+
+# open and read csv
+with open(budget_data, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+    # skip header row
+    print(f"Header: {csv_header}")
+
+    # find net amount of profit and loss
+    P = []
+    months = []
+
+    #read through each row of data after header
+    for rows in csvreader:
+        P.append(int(rows[1]))
+        months.append(rows[0])
+
+    # find revenue change
+    revenue_change = []
+
+    for x in range(1, len(P)):
+        revenue_change.append((int(P[x]) - int(P[x-1])))
+    
+    # calculate average revenue change
+    revenue_average = sum(revenue_change) / len(revenue_change)
+    
+    # calculate total length of months
+    total_months = len(months)
+
+    # greatest increase in revenue
+    greatest_increase = max(revenue_change)
+    # greatest decrease in revenue
+    greatest_decrease = min(revenue_change)
+
+
+    # print the Results
+    print("Financial Analysis")
+
+    print("....................................................................................")
+
+    print("total months: " + str(total_months))
+
+    print("Total: " + "$" + str(sum(P)))
+
+    print("Average change: " + "$" + str(revenue_average))
+
+    print("Greatest Increase in Profits: " + str(months[revenue_change.index(max(revenue_change))+1]) + " " + "$" + str(greatest_increase))
+
+    print("Greatest Decrease in Profits: " + str(months[revenue_change.index(min(revenue_change))+1]) + " " + "$" + str(greatest_decrease))
+
